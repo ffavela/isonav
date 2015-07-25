07e39740ec6ffc455ee684025b7f176bab286b2c
frank <ffavela@gmail.com>
2015-07-24 22:47:01 -0500

    Moved sume supplenentary functions from isonav here

---
+++ misc.py
@@ -0,0 +1,65 @@
+#Some miscellaneous functions
+#May need to update them
+
+def firstNoQNeg(val=5):
+    fQDict={}
+    for i in range(117):
+        k=getKey(i+1)
+        print i+1,k
+        if k==False:
+            continue
+        for iso in iDict[k][1]:
+            isoVal=str(iso)+k
+            d=QStable(isoVal)
+        if d==False:
+            continue
+        if d!=[]:
+            fQDict[k]=[i+1,iso]
+            val-=1
+            print "Here again!",iso,d
+            print iso #,d
+        if val<=0:
+            return fQDict
+    return fQDict
+
+def numberReact(iso1):
+    for e in iDict:
+        for i in iDict[e][1]:
+            print e,i
+            iso2=str(i)+e
+            nR=nReaction(iso1,iso2)
+            if nR==False:
+                print 0
+            else:
+                print len(nR)
+
+
+#Print out the first unstable elements
+#(in terms of Q)
+def firstQPos(val=5):
+    fQListX=[]
+    fQListY=[]
+    for i in range(117):
+        k=getKey(i+1)
+        print i+1,k
+        if k==False:
+            continue
+        for iso in iDict[k][1]:
+            isoVal=str(iso)+k
+            d=QDecay(isoVal)
+            if d==False:
+                continue
+            if d!=[]:
+                fQListX.append(i+1)
+                fQListY.append(iso)
+                val-=1
+                print "Here!",iso #,d
+                print iso #,d
+                break
+            else:#Just testing
+                fQListX.append(i+1)
+                fQListY.append(0)
+            if val<=0:
+                return fQListX,fQListY
+    return fQListX,fQListY
+
