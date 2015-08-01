e89490651a640b007b3c96d4bea049cdd7c6bec7
frank <ffavela@gmail.com>
2015-07-31 22:43:01 -0500

    Simple script for testing functionality.

---
+++ isonavScripts/testingScript.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#Just a series of tests, not very descriptive for now
+
+echo "isonav"
+isonav
+echo ""
+echo "isonav 22 -s"
+isonav 22 -s
+echo ""
+echo "isonav Au --protons"
+isonav Au --protons
+echo ""
+echo "isonav 195Au --neutrons"
+isonav 195Au --neutrons
+echo ""
+echo "isonav Pb -i"
+isonav Pb -i
+echo ""
+echo "isonav Pb -iv"
+isonav Pb -iv
+echo ""
+echo "isonav Au -im --amu"
+isonav Au -im --amu
+echo ""
+echo "isonav 22Ne --mass"
+isonav 22Ne --mass
+echo ""
+echo "isonav 22Ne --mass --liquidDrop"
+isonav 22Ne --mass --liquidDrop
+echo ""
+echo "isonav 22Ne --mirror"
+isonav 22Ne --mirror
+echo ""
+echo "isonav 40Ca -r"
+isonav 40Ca -r
+echo ""
+echo "isonav d 14N a 12C --Elab=3.0 --angle=35"
+isonav d 14N a 12C --Elab=3.0 --angle=35
+echo ""
+echo "isonav d 14N a 12C --Elab=3.0 --angle=35 --xTreme"
+isonav d 14N a 12C --Elab=3.0 --angle=35 --xTreme
+echo ""
+echo "isonav d 14N d 14N --Elab=5.5 --angle=25 --xTreme"
+isonav d 14N d 14N --Elab=5.5 --angle=25 --xTreme
+echo ""
+echo "isonav d 14N --reactions"
+isonav d 14N --reactions
+echo ""
+echo "isonav p 14N  --fussion"
+isonav p 14N  --fussion
+echo ""
+echo "isonav p 14N --fussion --Elab=0.1 -v"
+isonav p 14N --fussion --Elab=0.1 -v
+echo ""
+echo "isonav d 14N --Elab=3.0 --angle=35"
+isonav d 14N --Elab=3.0 --angle=35
+echo ""
+echo "isonav d 14N --Elab=3.0 --angle=35 --xTreme"
+isonav d 14N --Elab=3.0 --angle=35 --xTreme
+echo ""
+echo "isonav n --compton -v"
+isonav n --compton -v
+echo ""
+
