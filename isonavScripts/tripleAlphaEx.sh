f90b8e1f4a0ab13fea9cd756cadcdeccffb76913
Francisco Favela <ffavela@gmail.com>
2016-09-18 14:50:32 -0500

    updated for new syntax

--- isonavScripts/tripleAlphaEx.sh
+++ isonavScripts/tripleAlphaEx.sh
@@ -8,31 +8,36 @@ E1=5.15
 E2=5.48
 E3=5.8
 
-silThick=11
-goldThick=1.15
+silThick=11.5
+goldThick=1.4
 
-E1i=$(isonav a --material=silicon --thick=$silThick --Elab=$E1)
-dE1i=$(isonav a --material=silicon --thick=$silThick --Elab=$E1 --depo)
+echo "Calculating energy loss from a triple alpha source through"
+echo "a silicon detector with thickness $silThick microns and a gold"
+echo "and a gold coating of $goldThick microns."
 
-E2i=$(isonav a --material=silicon --thick=$silThick --Elab=$E2)
-dE2i=$(isonav a --material=silicon --thick=$silThick --Elab=$E2 --depo)
+E1i=$(isonav a --material=Si --thick=$silThick --Elab=$E1)
+dE1i=$(isonav a --material=Si --thick=$silThick --Elab=$E1 --depo)
 
-E3i=$(isonav a --material=silicon --thick=$silThick --Elab=$E3)
-dE3i=$(isonav a --material=silicon --thick=$silThick --Elab=$E3 --depo)
+E2i=$(isonav a --material=Si --thick=$silThick --Elab=$E2)
+dE2i=$(isonav a --material=Si --thick=$silThick --Elab=$E2 --depo)
 
-E1f=$(isonav a --material=gold --thick=$goldThick --Elab=$E1i)
-dE1f=$(isonav a --material=gold --thick=$goldThick --Elab=$E1i --depo)
+E3i=$(isonav a --material=Si --thick=$silThick --Elab=$E3)
+dE3i=$(isonav a --material=Si --thick=$silThick --Elab=$E3 --depo)
 
-E2f=$(isonav a --material=gold --thick=$goldThick --Elab=$E2i)
-dE2f=$(isonav a --material=gold --thick=$goldThick --Elab=$E2i --depo)
+E1f=$(isonav a --material=Au --thick=$goldThick --Elab=$E1i)
+dE1f=$(isonav a --material=Au --thick=$goldThick --Elab=$E1i --depo)
 
-E3f=$(isonav a --material=gold --thick=$goldThick --Elab=$E3i)
-dE3f=$(isonav a --material=gold --thick=$goldThick --Elab=$E3i --depo)
+E2f=$(isonav a --material=Au --thick=$goldThick --Elab=$E2i)
+dE2f=$(isonav a --material=Au --thick=$goldThick --Elab=$E2i --depo)
+
+E3f=$(isonav a --material=Au --thick=$goldThick --Elab=$E3i)
+dE3f=$(isonav a --material=Au --thick=$goldThick --Elab=$E3i --depo)
 
 tdE1=$(echo "scale=2; $dE1i+$dE1f" | bc -l)
 tdE2=$(echo "scale=2; $dE2i+$dE2f" | bc -l)
 tdE3=$(echo "scale=2; $dE3i+$dE3f" | bc -l)
 
-echo "The deposited energy for the $E1 MeV alpha is $tdE1 the expected is 2.55"
-echo "The deposited energy for the $E2 MeV alpha is $tdE2 the expected is 2.41"
-echo "The deposited energy for the $E3 MeV alpha is $tdE3 the expected is 2.29"
+echo -e "Alpha_E\tDeltaE\texp DeltaE"
+echo -e "$E1\t$tdE1\t2.55"
+echo -e "$E2\t$tdE2\t2.41"
+echo -e "$E3\t$tdE3\t2.29"
