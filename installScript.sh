ee18fe9775e506228581cca79a3a5a99e7bb1e3c
Francisco Favela <ffavela@gmail.com>
2016-10-01 13:09:11 -0500

    Automatic database download.

--- installScript.sh
+++ installScript.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 #Run the script as with sudo
 RED='\033[0;31m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+databaseName=data1p4p5
+zipFile=$databaseName.zip
+ggURLVal='https://drive.google.com/uc?export=download'
+dataID='0B2znJ2THUGDiWU5BYkFIVUdkdFk'
+dbURL="https://drive.google.com/open?id=0B2znJ2THUGDiWU5BYkFIVUdkdFk"
+
+function downloadDatabase(){
+    ggID=$dataID
+    ggURL=$ggURLVal
+    curl -LOJb /tmp/gcokie "${ggURL}&id=${ggID}"
+}
 
 if [ "$1" = "--uninstall" ]
 then
@@ -33,24 +46,42 @@ then
     printf "${RED}Error; install docopt first. Aborting${NC}\n"
     echo "For ubuntu execute"
     echo "sudo apt-get install python-docopt"
+    echo "For arch:"
+    echo "sudo pacman -S python-docopt"
     echo "For linux in general:"
     echo "pip install docopt==0.6.1"
     exit 1
 fi
 
-if [ ! -d data1p4p5 ]
+[ ! -d $databaseName ]&&[ -e $zipFile ]&& unzip $zipFile
+
+[ -e $zipFile ]&&[ -d $databaseName ]&& echo "rm $zipFile to avoid errors"&&rm $zipFile
+
+if [ ! -d $databaseName ]
 then
-    printf "${RED}Error; no data directory${NC}\n"
-    echo "Download the new database from:"
-    echo "https://drive.google.com/open?id=0B2znJ2THUGDiWU5BYkFIVUdkdFk"
-    echo "And uncompress it inside the repo's directory"
-    exit 2
+    printf "${BLUE}Trying to download database from:${NC}\n"
+    echo "$dbURL"
+    downloadDatabase
+    exitVal=$?
+    if [ $exitVal -ne 0 ]
+    then
+	printf "${RED}Error downloading database from:${NC}\n"
+	echo "$dbURL"
+	printf "${BLUE}Do you have internet connection?:${NC}\n"
+	printf "${GREEN}Perhaps google drive is blocked in${NC}\n"
+	printf "${GREEN}your area or it is down.${NC}\n"
+	printf "${BLUE}Please try again or download it manually from:${NC}\n"
+	echo "$dbURL"
+	echo "and uncompress it in this directory."
+	exit 2
+    fi
+    unzip $zipFile && echo "removing $zipFile" && rm $zipFile
 fi
 
 mkdir /usr/share/isonav
 [ $? -ne 0 ] && echo "Error; try running with sudo" && exit 3
 chmod +x isonav.py
-cp -r data1p4p5 /usr/share/isonav
+cp -r $databaseName /usr/share/isonav
 cp isonav.py /usr/share/isonav
 cp isoParser.py /usr/share/isonav
 cp isonavBase.py /usr/share/isonav
