0c77eb24e1eb775f977d211d0d7589ccc858779d
Francisco Favela <ffavela@gmail.com>
2017-10-11 20:25:40 +0200

    reduced the number of projectiles

--- isonavScripts/findReact.sh
+++ isonavScripts/findReact.sh
@@ -6,6 +6,7 @@ iso2Find=26Al #The isotope 2 find
 achiE=10.0 #CM Energy achievable at our lab
 maxPro=14
 maxTar=30
+minQ=-2.5
 
 pNum=$(isonav $iso2Find -p)
 initVal=$(echo "$pNum-1"|bc)
@@ -15,39 +16,31 @@ function getIsotopes {
     echo $iso | tr " " "\n"
 }
 
+isoPro=("1n" "1H" "2H" "3He" "4He" "6Li" "7Li")
+
 echo -e "#proj\ttarget\teje\tres\tthres\tQ\tcoulAft\tcoulb4"
-for n in $(seq 0 $maxPro)
+
+for iso1 in "${isoPro[@]}"
 do
-    s1=$(isonav $n -s)
-    isos1=$(getIsotopes $s1)
-    flagVar=0
     for m in $(seq $initVal $maxTar)
     do
         s2=$(isonav $m -s)
         isos2=$(getIsotopes $s2)
 
-        for iso1 in $isos1
+        for iso2 in $isos2
         do
-            for iso2 in $isos2
-            do
-                coulE=$(isonav $iso1 $iso2 --coulomb)
-                [ $(echo "$coulE > $achiE" | bc) -eq 1 ] && continue
-                flagVar=1 #At least one iso of s2 is under the barrier
-                val=$(isonav $iso1 $iso2 --reactions | grep $iso2Find)
-                if [ "$val" != "" ]
-                then
-                    Q=$(echo $val | cut -f4 -d' ')
-                    exitCoul=$(echo $val | cut -f5 -d' ')
-                    [ $(echo "$Q > 0" | bc) -eq 0 ] && continue
-                    coulEF=$(printf "%0.2f\n" $(bc -q <<< scale=2\;$coulE))
-                    echo -e "$iso1\t$iso2\t$val\t$coulEF"
-                fi
-            done
+            coulE=$(isonav $iso1 $iso2 --coulomb)
+            [ $(echo "$coulE > $achiE" | bc) -eq 1 ] && continue
+            flagVar=1 #At least one iso of s2 is under the barrier
+            val=$(isonav $iso1 $iso2 --reactions | grep $iso2Find)
+            if [ "$val" != "" ]
+            then
+                Q=$(echo $val | cut -f4 -d' ')
+                exitCoul=$(echo $val | cut -f5 -d' ')
+                [ $(echo "$Q > $minQ" | bc) -eq 0 ] && continue
+                coulEF=$(printf "%0.2f\n" $(bc -q <<< scale=2\;$coulE))
+                echo -e "$iso1\t$iso2\t$val\t$coulEF"
+            fi
         done
     done
-    if [ $flagVar = 0 ]
-    then
-        echo "No more reactions under the barrier"
-        exit 0
-    fi
 done
