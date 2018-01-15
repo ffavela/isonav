8625703edbb7052279f7cb4a1be89b147cc69696
Francisco Favela <ffavela@gmail.com>
2018-01-15 16:56:14 -0600

    Updated license

--- isonav.py
+++ isonav.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#   Copyright (C) 2015, 2016, 2017 Francisco Favela
+#   Copyright (C) 2015, 2016, 2017, 2018 Francisco Favela
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
