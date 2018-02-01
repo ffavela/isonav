212a48c2ca9a821cf09c7152ebac5eff7fbe4c56
Francisco Favela <ffavela@gmail.com>
2018-02-01 23:35:26 +0100

    improved color function 4 bincor

--- isonavScripts/binCor/chimeraGL.py
+++ isonavScripts/binCor/chimeraGL.py
@@ -7,6 +7,9 @@ from OpenGL.GLU import *
 from binCorComplement import *
 
 from random import random
+from math import *
+
+from miscellaneous import *
 
 colors = (
     (1,0,0),
@@ -39,13 +42,13 @@ edges = (
     )
 
 surfaces = (
-    (0,1,2,3),
-    (0,3,7,4),
-    (0,1,5,4),
+    # (0,1,2,3),
+    # (0,3,7,4),
+    # (0,1,5,4),
 
-    (6,2,3,7),
+    # (6,2,3,7),
     (6,7,4,5),
-    (6,5,1,2),
+    # (6,5,1,2),
     )
 
 # theta_min_val=radians(163)
@@ -62,14 +65,14 @@ def getDetValues(detIdx,subIdx):
         theta_min_val=radians(theta_min[detIdx])
         theta_max_val=radians(theta_max[detIdx])
         delta_phi_val=radians(delta_phi[detIdx])
-        dDist=det_dist[detIdx]/100.0
+        dDist=det_dist[detIdx]/50.0
         shift=delta_phi_val*subIdx
         detValDict[mySimpleStr]=theta_min_val,theta_max_val,delta_phi_val,dDist,shift
     return detValDict[mySimpleStr]
 
 vertValDict={}
 
-def getVerticies(dDist,theta_min_val,theta_max_val,delta_phi_val,shift):
+def getVerticies4Telescope(dDist,theta_min_val,theta_max_val,delta_phi_val,shift):
     myAwesomeStr=str([dDist,theta_min_val,theta_max_val,delta_phi_val,shift])
     if myAwesomeStr not in vertValDict:
         delta_r=0.1
@@ -87,7 +90,7 @@ def getVerticies(dDist,theta_min_val,theta_max_val,delta_phi_val,shift):
             (dDist*sin(theta_min_val)*cos(delta_phi_val/2+shift),
              dDist*sin(theta_min_val)*sin(delta_phi_val/2+shift),
              dDist*cos(theta_min_val)),
-        
+
             (dDDist*sin(theta_max_val)*cos(delta_phi_val/2+shift),
              dDDist*sin(theta_max_val)*sin(delta_phi_val/2+shift),
              dDDist*cos(theta_max_val)),
@@ -104,55 +107,96 @@ def getVerticies(dDist,theta_min_val,theta_max_val,delta_phi_val,shift):
         vertValDict[myAwesomeStr]=verticies
     return vertValDict[myAwesomeStr]
 
-def drawSurfaces(verticies):
+def drawSurfaces(verticies,t=0.999):
     glBegin(GL_QUADS)
     for surface in surfaces:
         x = 0
         for vertex in surface:
             x+=1
-            glColor3fv(colors[x])
+            # glColor3fv(colors[x])
+            # glColor3fv((1,0,0))
+            # myColor=(cos(t*pi/2)**2,0,sin(t*pi/2)**2)
+            # myColor=(random(),random(),random())
+            myColor=convert_to_rgb(0,1,t)
+            # myColor=getRgb(0,1,t)
+            # print("t = ", t )
+            # cmap = cm.autumn
+            # myColor=cmap(t)[:3]
+            glColor3fv(myColor)
             glVertex3fv(verticies[vertex])
+            glColor3fv((1,1,1))
     glEnd()
-    
-def drawChimTelesGL(detIdx,subIdx):
+
+def drawChimTelesGL(detIdx,subIdx,surfStat=False,t=0):
     theta_min_val,theta_max_val,delta_phi_val,dDist,shift=getDetValues(detIdx,subIdx)
-    verticies=getVerticies(dDist,theta_min_val,theta_max_val,delta_phi_val,shift)
-    # drawSurfaces(verticies)
-    
+    verticies=getVerticies4Telescope(dDist,theta_min_val,theta_max_val,delta_phi_val,shift)
+    if surfStat:
+        drawSurfaces(verticies,t)
+
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
             glVertex3fv(verticies[vertex])
     glEnd()
 
-def drawRing(detIdx):
+def drawChimTelesGL2(detIdx,subIdx,surfStat=False):
+    theta_min_val,theta_max_val,delta_phi_val,dDist,shift=getDetValues(detIdx,subIdx)
+    verticies=getVerticies4Telescope(dDist,theta_min_val,theta_max_val,delta_phi_val,shift)
+    if surfStat:
+        drawSurfaces(verticies,random())
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+
+def drawRing(detIdx,subTDict):
     telesN=teles_num[detIdx]
     for subIdx in range(telesN):
-        drawChimTelesGL(detIdx,subIdx)
+        tVal=subTDict[subIdx]
+        drawChimTelesGL(detIdx,subIdx,True,tVal)
 
-def drawAllChimera():
+def drawAllChimera(tDict):
     for i in range(34):
-        drawRing(i)
+    # for i in range(24):
+        ringT=ring_tags[i]
+        subTDict=tDict[ringT]
+        drawRing(i,subTDict)
+
+def getTDict(rStr,sTel):
+    aDict=getRelAngleDict(rStr,sTel)
+    tDict={}
+    for ringStr in aDict:
+        tDict[ringStr]=[]
+        for subTel in aDict[ringStr]:
+            tVal=subTel/180
+            tDict[ringStr].append(tVal)
+    return tDict
 
 def main():
+    tDict=getTDict("S19",18)
+    # print(tDict)
     pygame.init()
-    display = (800,600)
+    display = (1900,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
     glTranslatef(0.0,0.0, -5)
-
+    glRotatef(120, 0, 1, 0)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
+        glRotatef(1, 0, 1, 0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        # drawChimTelesGL(25,5)
-        drawAllChimera()
+        drawAllChimera(tDict)
+        # drawChimTelesGL(25,5,True)
+        # drawChimTelesGL2(32,20,True)
         # drawRing(15)
         pygame.display.flip()
         pygame.time.wait(10)
---
+++ isonavScripts/binCor/miscellaneous.py
@@ -0,0 +1,23 @@
+import sys
+EPSILON = sys.float_info.epsilon  # smallest possible difference
+
+def convert_to_rgb(minval, maxval, val):
+    colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]  # [BLUE, GREEN, RED]
+    fi = float(val-minval) / float(maxval-minval) * (len(colors)-1)
+    i = int(fi)
+    f = fi - i
+    if f < EPSILON:
+        return [c/255.0 for c in colors[i]]
+    else:
+        (r1, g1, b1), (r2, g2, b2) = colors[i], colors[i+1]
+        # return (b1+f*(b2-b1))/255,(g1+f*(g2-g1))/255,r1+f*(r2-r1))/255
+        return (b1+f*(b2-b1))/255,(g1+f*(g2-g1))/255,(r1+f*(r2-r1))/255
+
+def getRgb(minimum, maximum, value):
+    minimum, maximum = float(minimum), float(maximum)
+    ratio = 2 * (value-minimum) / (maximum - minimum)
+    b = int(max(0, 255*(1 - ratio)))
+    r = int(max(0, 255*(ratio - 1)))
+    g = 255 - b - r
+    return r, g, b
+
