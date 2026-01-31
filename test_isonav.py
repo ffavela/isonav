04ee07af38799fdd879f4d139765342673b5991d
Frank <ffavela@gmail.com>
2026-01-31 13:41:00 -0600

    A TODO so I don't forget

--- test_isonav.py
+++ test_isonav.py
@@ -1,3 +1,6 @@
+
+# TODO: be sure to have a diff comparison between expectedBehavior.txt
+# and the output from testingScript.sh
 def test_dummy():
     """Just a dumb first assertion"""
     assert 1 == 1
